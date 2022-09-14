2d3d187b10a107a594231163e4a01357a55d10eb
zentred <87871486+zentred@users.noreply.github.com>
2022-09-14 09:02:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,68 @@
-import requests
-import re
-from collections import Counter
-req = requests.Session()
-import time
+import requests, json, time, random, datetime
+from threading import Thread
 
-lst = []
+with open('config.json') as config:
+    config = json.load(config)
 
-comparer = 0
-hourly = 0
-daily = 0
-sales_daily = 0
-sales_hourly = 0
-full_id = 0
+with requests.Session() as session:
+    session.cookies['.ROBLOSECURITY'] = config['cookie']
 
-webhook = 'put your webhook here'
-groupid = 'put groupid here'
-cookie = 'put cookie here'
+class Group():
 
-def sales():
-    req.cookies['.ROBLOSECURITY'] = cookie
-    r = req.get(f'https://economy.roblox.com/v2/groups/{groupid}/transactions?cursor=&limit=100&sortOrder=Asc&transactionType=Sale').json()['data']
-    c = 0
-    for i in range(50):
-        t = r[c]
-        saleid = t['id']
-        userid = t['agent']['id']
-        username = t['agent']['name']
-        clothingid = t['details']['id']
-        clothing_name = t['details']['name']
-        price = t['currency']['amount']
-        lst.append(f'{username}:{userid}:{clothing_name}:{price}:{saleid}:{clothingid}')
-        c += 1
+    def __init__(self):
+        pass
 
-def number(lst):
-    n = lst.split(':')[4]
-    return int(n)
+    def initialSales(self):
+        response = session.get(f'https://economy.roblox.com/v2/groups/{config["group_id"]}/transactions?cursor=&limit=100&sortOrder=Asc&transactionType=Sale').json()['data']
+        self.previousSales = [sale['id'] for sale in response]
 
-def check():
-    global sales_hourly
-    global sales_daily
-    global hourly
-    global daily
-    global comparer
+    def getHourly(self, response):
+        hourlySales = hourlyGain = 0
+        currentUnix = int(time.time())
+        for sale in response:
+            date = sale['created'].split('.')[0].replace('T', ' ')
+            dt = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+            saleUnix = time.mktime(dt.timetuple())
+            if currentUnix-3600 <= saleUnix+3600: # i don't know why everytime i convert these sale times to unix they have -3600 less, but anyways i just +3600 to fix that i guess
+                hourlySales += 1
+                hourlyGain += sale['currency']['amount']
+        return hourlySales, hourlyGain
 
-    lst.sort(key=number)
+    def constantCheck(self):
+        while True:
+            try:
+                response = session.get(f'https://economy.roblox.com/v2/groups/{config["group_id"]}/transactions?cursor=&limit=100&sortOrder=Asc&transactionType=Sale').json()['data']
 
-    counts = Counter(lst)
-    for line in counts:
-        if counts[line] == 1:
-            time.sleep(0.1)
-            sales_hourly += 1
-            sales_daily += 1
-            user, userid, clothing_name, price, saleid, clothingid = line.split(':',6)
-            data = {
-                  'embeds':[{
-                      'author': {
-                          'name': f'Asset Sold'
-                          },
-                      'color': int('0099E1',16),
-                      'fields': [
-                          {'name': '\u200b', 'value': f'**User**: {user}\n**UserID**: {userid}\n**Price**: {price}', 'inline':False},
-                          {'name': '\u200b', 'value': f'**Daily Sales**: {sales_daily}\n**Hourly Sales**: {sales_hourly}', 'inline':False},
-                          {'name': '\u200b', 'value': f'**Asset**: {clothing_name}\n**Asset ID**: {clothingid}', 'inline':False},
-                      ],
-                      'thumbnail': {
-                          'url': f'https://www.roblox.com/headshot-thumbnail/image?userId={userid}&width=420&height=420&format=png',
-                          }
-                }]
-              }
-                r = requests.post(webhook,json=data).text
-            else: pass
+                hourlySales, hourlyGain = self.getHourly(response)
 
-sales()
-time.sleep(60)
+                for sale in response:
+                    if sale['id'] not in self.previousSales:
 
+                        data = {
+                            'embeds':[{
+                                'author': {
+                                    'name': f'{sale["details"]["name"]}'
+                                    },
+                                'color': int('0099E1',16),
+                                'fields': [
+                                    {'name': 'Buyer', 'value': f'{sale["agent"]["name"]}', 'inline':True},
+                                    {'name': 'Price', 'value': f'{sale["currency"]["amount"]}', 'inline':True},
+                                    {'name': 'Statistics', 'value': f'Past Hour Sales: {hourlySales}\nPast Hour Gain: R$ {hourlyGain}', 'inline':False},
+                                ],
+                                'thumbnail': {
+                                    'url': f'https://www.roblox.com/headshot-thumbnail/image?userId={sale["agent"]["id"]}&width=420&height=420&format=png',
+                                    }
+                            }]
+                        }
+                        requests.post(config['webhook'], json=data)
 
-while True:
-    sales()
-    check()
-    print('checked')
-    time.sleep(60)
-    
-    if daily % 60 == 0:
-        daily -= daily
-    else: pass
+                self.previousSales = [sale['id'] for sale in response]
+                time.sleep(60)
 
-    if hourly % 1440 == 0:
-        hourly -= hourly
-    else: pass
-    # i dont know if the reset sales count hourly and daily even works properly
+            except:
+                time.sleep(5)
+                pass
+
+a = Group()
+a.initialSales()
+a.constantCheck()
